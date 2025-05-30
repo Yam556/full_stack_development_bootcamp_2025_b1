f25f667b5e5f66040305f6da4a9836b31dd46790
Yam Khadka <165254553+Yam556@users.noreply.github.com>
2025-05-30 20:40:25 +1000

    update in js

---
+++ js_01/app.js
@@ -0,0 +1,51 @@
+/* 
+Literals - to check literals use -typeof in console */
+
+// 01. string literals --anytghing inside single or double quotation
+//--"sandesh", "welcome to nepal", "b";
+
+// 02. Numeric literals -- numbers
+// -- 100, 202.22
+
+//Boolean literals
+//true or false result
+
+// Array literals
+// ["sunday", 'monday', "tuesday", 122, false]
+
+//object literals {}
+
+/* 
+
+key: value
+
+object{
+firstName: "Yam",
+age: 26, 
+degree: "Bachelor"
+isStudent: yes
+address: {
+street: "haris street",
+unit: 10,
+state: "NSW"
+} object inside object can be created in above method
+}
+*/
+//regEx literals
+// --- /a-zA-Z0-9/
+/*
+let regex = /hello/i;  // Matches 'hello', 'Hello', 'HELLO', etc.
+let result = regex.test("Hello World");
+console.log(result); // true */
+
+//variable
+//declaration
+//assignment
+
+var firstnumber;
+firstnumber = 500;
+console.log('This is the highest number: ' + firstnumber);
+
+let phone = '0426733324';
+console.log('my phone number is: ' + phone);
+console.log(phone);
